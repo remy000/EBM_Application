f0351232fb4261d573e577f913755b83b3d780bf
remy000 <dukundaneremy2001@gmail.com>
2024-03-21 14:52:06 +0200

    add new features

--- src/main/java/com/rra/ebm/EBMApplication/service/RequestService.java
+++ src/main/java/com/rra/ebm/EBMApplication/service/RequestService.java
@@ -1,4 +1,69 @@
 package com.rra.ebm.EBMApplication.service;
 
+import com.rra.ebm.EBMApplication.domain.Requests;
+import com.rra.ebm.EBMApplication.repository.RequestRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+@Service
 public class RequestService {
+    @Value("${upload.path}")
+    private String uploadPath;
+    private RequestRepo requestRepo;
+
+    @Autowired
+    public RequestService(RequestRepo requestRepo) {
+        this.requestRepo = requestRepo;
+    }
+
+    public void saveRequest(Requests requests, MultipartFile letter, MultipartFile certifcate,
+                            MultipartFile vatCertificate, MultipartFile idCard){
+        Requests req=requestRepo.save(requests);
+
+        try{
+            if(letter!=null){
+                String letterPath=saveFile(letter);
+                req.setLetterPath(letterPath);
+
+            }
+            if(certifcate!=null){
+                String certPath=saveFile(certifcate);
+                req.setCertPath(certPath);
+            }
+            if(vatCertificate!=null){
+                String vathPath=saveFile(vatCertificate);
+                req.setVatPath(vathPath);
+
+            }
+            if(idCard!=null){
+                String idPath=saveFile(idCard);
+                req.setIdPath(idPath);
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+
+
+        }
+    }
+    private String saveFile(MultipartFile file) throws IOException {
+        String fileName= UUID.randomUUID().toString()+""+file.getOriginalFilename();
+        Path imagesDirectory= Paths.get(uploadPath);
+        if(!Files.exists(imagesDirectory)){
+            Files.createDirectories(imagesDirectory);
+        }
+        Path filePath=imagesDirectory.resolve(fileName);
+        Files.copy(file.getInputStream(),filePath);
+        return fileName;
+
+    }
 }
